4c1fa0b704df1e6505f4e6a4da9d0fae0c1f7d1e
David Hjelm <>
2022-12-09 11:07:30 +0100

    Fixa bugg i dubletter

--- d07p01.py
+++ d07p01.py
@@ -1,11 +1,8 @@
 f = open("inputs/d07", "r")
 
 
-def get_dir_by_path(dir_list, path):
-    cur_d = dir_list
-    for k in path:
-        cur_d = cur_d.get_dir(k)
-    return cur_d
+def get_dir_by_path():
+    return root_dir.get_dir()
 
 
 class File:
@@ -21,10 +18,9 @@ class Directory:
         self.files = []
 
     def add_dir(self, name):
-        for dir in self.subdirs:
-            if dir.name == name:
-                return
-        new_dir = Directory(name)
+        cur_path_cpy = current_path.copy()
+        cur_path_cpy.append(name)
+        new_dir = Directory(cur_path_cpy)
         self.subdirs.append(new_dir)
 
     def add_file(self, name, size):
@@ -34,13 +30,13 @@ class Directory:
         new_file = File(name, size)
         self.files.append(new_file)
 
-    def get_dir(self, name):
-        if self.name == name:
+    def get_dir(self):
+        if current_path == self.name:
             return self
         else:
             for dir in self.subdirs:
-                if dir.name == name:
-                    return dir
+                if dir.get_dir() != None:
+                    return dir.get_dir()
 
     def get_dir_size(self):
         total_size = 0
@@ -51,7 +47,7 @@ class Directory:
         return total_size
 
 
-root_dir = Directory("/")
+root_dir = Directory(["/"])
 list_mode = False
 
 current_path = ["/"]
@@ -62,7 +58,7 @@ for line in f.readlines():
         if line.startswith("$"):
             list_mode = False
         else:
-            cur_dir = get_dir_by_path(root_dir, current_path)
+            cur_dir = get_dir_by_path()
             if line.startswith("dir"):
                 dir_line = line.split(" ")
                 cur_dir.add_dir(dir_line[1])
@@ -83,10 +79,11 @@ for line in f.readlines():
 
 def scan_dir(dir):
     sum = 0
+    size = dir.get_dir_size()
+    if size < 100000:
+        sum += size
     for d in dir.subdirs:
         size = d.get_dir_size()
-        if size < 100000:
-            sum += size
         sum += scan_dir(d)
     return sum
 
